55776e6f3cb2975d5a14ec9746e7dab9a0afa066
MilaMi94 <mila.bogdanovic94@gmail.com>
2023-02-21 19:39:54 +0100

    select

--- Hooks/hooks/src/components/Select.js
+++ Hooks/hooks/src/components/Select.js
@@ -1,35 +1,56 @@
 import React, { useEffect, useState } from "react";
 
 const Select = () => {
-  const [select, setSelect] = useState("posts");
+  const [state, setState] = useState("posts");
   const [items, setItems] = useState([]);
-  /*
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((json) => setItems(json));*/
-  //Prilikom renderovanja komponente, uradite API poziv ka
-  //https://jsonplaceholder.typicode.com/ .I niz koji dobijete upisite u vas state koji se naziva items
 
   const handleSelect = (e) => {
-    setSelect((prevState) => (prevState = e.target.value));
+    setState(e.target.value);
   };
 
-  // Prilikom promene state-a koji kontrolice select value (koristeci useEffect method), pozovite ponovu vas API poziv
+  //Prilikom renderovanja komponente, uradite API poziv ka
+  //https://jsonplaceholder.typicode.com/ .I niz koji dobijete upisite u vas state koji se naziva items
+  useEffect(() => {
+    console.log("Komponenta se renderovala");
+    fetch(`https://jsonplaceholder.typicode.com/${state}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json))
+      .catch((err) => console.log(err));
+  }, []); // prazan string drugi argument jer zelimo prilikom renderovanja
+
+  // Prilikom promene state-a koji kontrolice select value (koristeci useEffect method), pozovite ponovo vas API poziv
+
+  useEffect(() => {
+    console.log("Komponenta se updejtovala");
+    fetch(`https://jsonplaceholder.typicode.com/${state}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [state]); // drugi argument pozivamo kad zelimo na update i tu stavljamo od cega nam zavisi update
 
   return (
-    <form>
-      <select onChange={handleSelect}>
-        <option value="posts">posts</option>
-        <option value="comments">comments</option>
-        <option value="albums">albums</option>
-      </select>
-
-      {/* <ul>
-        {items.map((user) => {
-          return <li key={user.id}>{user.name}</li>;
+    <div>
+      <form style={{ marginTop: 20 }}>
+        <select
+          style={{ borderRadius: 5 }}
+          value={state}
+          onChange={handleSelect}
+        >
+          <option value="posts">Posts</option>
+          <option value="comments">Comments</option>
+          <option value="albums">Albums</option>
+        </select>
+        {/* Ispisite items  u unorder list */}
+      </form>
+      <ul>
+        {items.map((item, index) => {
+          return (
+            <li style={{ margin: 10, color: "blue" }} key={index}>
+              {JSON.stringify(item)}
+            </li>
+          );
         })}
-      </ul> */}
-    </form>
+      </ul>
+    </div>
   );
 };
 
