aa9b2d7af351461e1a5a83e517e19de35b77806b
MilaMi94 <mila.bogdanovic94@gmail.com>
2023-02-21 16:20:03 +0100

    hooks

---
+++ Hooks/hooks/src/components/Select.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const Select = () => {
+  const [select, setSelect] = useState("post");
+  const [items, setItems] = useState([]);
+  /*
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((json) => setItems(json));*/
+  //Prilikom renderovanja komponente, uradite API poziv ka
+  //https://jsonplaceholder.typicode.com/ .I niz koji dobijete upisite u vas state koji se naziva items
+
+  const handleSelect = (e) => {
+    console.log(e.target.value);
+    setSelect((prevState) => (prevState = e.target.value));
+  };
+
+  // Prilikom promene state-a koji kontrolice select value (koristeci useEffect method), pozovite ponovu vas API poziv
+
+  return (
+    <form>
+      <select onChange={handleSelect}>
+        <option value="posts">posts</option>
+        <option value="comments">comments</option>
+        <option value="albums">albums</option>
+      </select>
+
+      {/* <ul>
+        {items.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul> */}
+    </form>
+  );
+};
+
+export default Select;
---
+++ vezbanja/hooks/hooks/src/components/Select.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+const Select = () => {
+  const [select, setSelect] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  /*fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => setItems());
+*/
+  //Prilikom renderovanja komponente, uradite API poziv ka
+  //https://jsonplaceholder.typicode.com/ .I niz koji dobijete upisite u vas state koji se naziva items
+
+  const handleSelect = (e) => {
+    console.log(items);
+    console.log(e.target.value);
+    setSelect((prevState) => (prevState = e.target.value));
+  };
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${select}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, []);
+
+  // Prilikom promene state-a koji kontrolice select value (koristeci useEffect method), pozovite ponovu vas API poziv
+
+  return (
+    <form>
+      <select onChange={handleSelect}>
+        <option value="posts">posts</option>
+        <option value="comments">comments</option>
+        <option value="albums">albums</option>
+      </select>
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.body}</li>;
+        })}
+      </ul>
+    </form>
+  );
+};
+
+export default Select;
